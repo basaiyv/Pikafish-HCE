3bbc89ba4d5d621a96454e4ade6139dbc11e538b
basaiyv <115393375+basaiyv@users.noreply.github.com>
2025-07-25 12:55:38 +0800

    Update position.cpp

--- src/position.cpp
+++ src/position.cpp
@@ -972,7 +972,7 @@ bool Position::rule_judge(Value& result, int ply) const {
     }
 
     // 60 move rule
-    if (st->rule60 >= 120)
+    if (st->rule60 >= 150)
     {
         result = MoveList<LEGAL>(*this).size() ? VALUE_DRAW : mated_in(ply);
         return true;
